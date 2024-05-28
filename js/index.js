b337ea16a94d6d894c7eea6949e0fb9fd39ad342
Tuancute1 <huynhminhtuan13032003@gmail.com>
2024-05-29 02:16:26 +0700

    xong

---
+++ js/index.js
@@ -0,0 +1,17 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+    loop:true,
+    infobar:true,
+    AnimationEffect: "rotate",
+    animationDuration: 1000,
+    transitionEffect: "zoom-in-out",
+    transitionDuration: 1000,
+    button: [
+        "zoom",
+        "share",
+        'slideShow',
+        "fullScreen",
+        "download",
+        "thumbs",
+        "close"
+    ]
+  });
